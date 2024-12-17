01dc9f3793361b8dd4457f53005e690b2aaeaf6d
Faixan6190 <faixan6190@gmail.com>
2024-12-17 22:11:50 +0500

    working on responsiveness

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Products = () => {
+  return (
+    <>
+        <div>
+            {/* Header Section */}
+            {/* Body Section */}
+        </div>
+    </>
+  )
+}
+
+export default Products
