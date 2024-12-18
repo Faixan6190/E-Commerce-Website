5e355a152a010c8f917c37d2d615d8ed0cdc6ef6
Faixan6190 <faixan6190@gmail.com>
2024-12-18 19:25:30 +0500

    working on products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,15 +1,73 @@
-import React from 'react'
+import React from "react";
+import Img1 from "../../assets/women/women.png";
+import Img2 from "../../assets/women/women2.jpg";
+import Img3 from "../../assets/women/women3.jpg";
+import Img4 from "../../assets/women/women4.jpg";
+
+const ProductsData = [
+  {
+    id: 1,
+    img: Img1,
+    title: "Women Ethnic",
+    rating: 5.0,
+    color: "white",
+    aosDelay: "0",
+  },
+  {
+    id: 2,
+    img: Img2,
+    title: "Women western",
+    rating: 4.5,
+    color: "Red",
+    aosDelay: "200",
+  },
+  {
+    id: 3,
+    img: Img3,
+    title: "Goggles",
+    rating: 4.7,
+    color: "brown",
+    aosDelay: "400",
+  },
+  {
+    id: 4,
+    img: Img4,
+    title: "Printed T-Shirt",
+    rating: 4.4,
+    color: "Yellow",
+    aosDelay: "600",
+  },
+  {
+    id: 5,
+    img: Img2,
+    title: "Fashin T-Shirt",
+    rating: 4.5,
+    color: "Pink",
+    aosDelay: "800",
+  },
+];
 
 const Products = () => {
   return (
-    <>
-        <div>
-            {/* Header Section */}
-            
-            {/* Body Section */}
+    <div className="mt-14 mb-12">
+      <div className="container">
+        {/* Header Section */}
+        <div className="text-center mb-10 max-w-[600px] mx-auto">
+          <p className="text-sm text-primary">Top Selling Products for you</p>
+          <h1 className="text-3xl font-bold">Products</h1>
+          <p className="text-xs text-gray-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, aspernatur elit. Fugit, aspernatur
+          </p>
         </div>
-    </>
-  )
-}
+        {/* Body Section */}
+        <div>
+          <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-5">
+            {/* card section */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Products
+export default Products;
