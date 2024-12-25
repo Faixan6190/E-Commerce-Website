e1570ffddfbd3c94ae83ad83b6997a86e30816b2
Faixan6190 <faixan6190@gmail.com>
2024-12-25 17:08:49 +0500

    complete Top Products

--- src/components/TopProducts/TopProducts.jsx
+++ src/components/TopProducts/TopProducts.jsx
@@ -34,7 +34,7 @@ const TopProducts = () => {
       <div className="container">
         {/* Header Section */}
         <div>
-          <div className="text-left mb-10">
+          <div className="text-left mb-24">
             <p className="text-sm text-primary" data-aos="fade-up">
               Top Rated Products for you
             </p>
@@ -49,12 +49,36 @@ const TopProducts = () => {
         {/* Body Section */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-20 md:gap-5 place-items-center">
           {ProductsData.map((data) => (
-            <div className="rounded-2xl bg-white dark:bg-gray-800 hover:bg-black/80 dark:hover:bg-primary hover:text-white relative shadow-xl duration-300 group max-w-[300px]">
+            <div
+              data-aos="zoom-in"
+              className="rounded-2xl bg-white dark:bg-gray-800 hover:bg-black/80 dark:hover:bg-primary hover:text-white relative shadow-xl duration-300 group max-w-[300px]"
+            >
               {/* image section */}
-              <div>
-                <img src={data.img} alt="" />
+              <div className="h-[100px]">
+                <img
+                  src={data.img}
+                  alt=""
+                  className="max-w-[140px] block mx-auto transform -translate-y-20 group-hover:scale-105 duration-300 drop-shadow-md"
+                />
               </div>
               {/* details section */}
+              <div className="p-4 text-center">
+                {/* star rating */}
+                <div className="w-full flex items-center justify-center gap-1">
+                  <FaStar className="text-yellow-500" />
+                  <FaStar className="text-yellow-500" />
+                  <FaStar className="text-yellow-500" />
+                  <FaStar className="text-yellow-500" />
+                </div>
+                <h1 className="text-xl font-bold">{data.title}</h1>
+                <p className="text-gray-500 group-hover:text-white duration-300 text-sm line-clamp-2">{data.description}</p>
+                <button
+                  className="bg-primary hover:scale-105 duration-300 text-white py-1 px-4 rounded-full mt-4 group-hover:bg-white group-hover:text-primary"
+                  // onClick={handleOrderPopup}
+                >
+                  Order Now
+                </button>
+              </div>
             </div>
           ))}
         </div>
