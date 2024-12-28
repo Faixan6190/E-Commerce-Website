948a14330444570cab068ff6fde8996d5ca368dc
Faixan6190 <faixan6190@gmail.com>
2024-12-28 16:05:22 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import Products from "./components/Products/Products";
 import TopProducts from "./components/TopProducts/TopProducts";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Banner from "./components/Banner/Banner";
+import Subscribe from "./components/Subscribe/Subscribe";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   React.useEffect(() => {
@@ -23,6 +27,11 @@ const App = () => {
       <Hero />
       <Products />
       <TopProducts />
+      <Banner />
+      <Subscribe />
+      <Products />
+      <Testimonials />
+      <Footer />
     </>
   );
 };
